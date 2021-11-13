1844957c91e2f860f001c34463118ebc0013498c
dclinee <dengchanglin8@outlook.com>
2021-11-13 18:53:05 +0800

    modified init.el in my ubuntu system

--- init.el
+++ init.el
@@ -171,6 +171,25 @@
 
 (provide 'init)
 
+
+
+;; Set up package.el to work with MELPA
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
+
+(package-refresh-contents)
+
+;; Download Evil
+(unless (package-installed-p 'evil)
+  (package-install 'evil))
+
+;; Enable Evil
+(require 'evil)
+(evil-mode 1)
+
+
+
 ;; Local Variables:
 ;; coding: utf-8
 ;; no-byte-compile: t
